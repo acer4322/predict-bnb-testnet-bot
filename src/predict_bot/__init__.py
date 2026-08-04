4a9823e0a83354b5202aa62cd43a670a5e6509d3
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-04 20:17:12 +0800

    Install confirmed Microprice live patch

--- src/predict_bot/__init__.py
+++ src/predict_bot/__init__.py
@@ -12,6 +12,9 @@ from .decision_snapshot_state import (
 from .direct_outcome_guard import (
     install_direct_outcome_guard as _install_direct_outcome_guard,
 )
+from .microprice_confirm_live_patch import (
+    install_microprice_confirm_live_patch as _install_microprice_confirm_live_patch,
+)
 from .microprice_feature_shadows import (
     install_microprice_feature_shadows as _install_microprice_feature_shadows,
 )
@@ -35,12 +38,14 @@ _install_microprice_variant_relaxation()
 _install_microprice_variants()
 _install_microprice_dashboard_patch()
 _install_microprice_feature_shadows()
+_install_microprice_confirm_live_patch()
 _install_confirmation_add_sources_patch()
 _install_pair_arb_initial_capacity_patch()
 del _install_confirmation_add_sources_patch
 del _install_decision_snapshot_diagnostics
 del _install_decision_snapshot_state
 del _install_direct_outcome_guard
+del _install_microprice_confirm_live_patch
 del _install_microprice_feature_shadows
 del _install_microprice_variant_relaxation
 del _install_microprice_variants
