3192f5879272907d3d4c82ebf9dcdf8ddc8d427d
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-06 03:26:37 +0800

    load minimum stake floor for loss streak guard

--- src/predict_bot/__init__.py
+++ src/predict_bot/__init__.py
@@ -24,6 +24,9 @@ from .loss_streak_guard_hotfix import (
 from .loss_streak_guard_patch import (
     install_loss_streak_guard_patch as _install_loss_streak_guard_patch,
 )
+from .loss_streak_minimum_stake_patch import (
+    install_loss_streak_minimum_stake_patch as _install_loss_streak_minimum_stake_patch,
+)
 from .microprice_confirm_exit_098_entry_guard_patch import (
     install_microprice_confirm_exit_098_entry_guard_patch as _install_microprice_confirm_exit_098_entry_guard_patch,
 )
@@ -97,6 +100,7 @@ _install_confirmation_add_sources_patch()
 _install_pair_arb_initial_capacity_patch()
 _install_loss_streak_guard_patch()
 _install_loss_streak_guard_hotfix()
+_install_loss_streak_minimum_stake_patch()
 del _install_calibrated_value_confirmation_filters
 del _install_calibrated_value_confirmation_variants
 del _install_confirmation_add_sources_patch
@@ -105,6 +109,7 @@ del _install_decision_snapshot_state
 del _install_direct_outcome_guard
 del _install_loss_streak_guard_hotfix
 del _install_loss_streak_guard_patch
+del _install_loss_streak_minimum_stake_patch
 del _install_microprice_confirm_exit_098_entry_guard_patch
 del _install_microprice_confirm_exit_098_horizon_patch
 del _install_microprice_confirm_exit_098_live_patch
