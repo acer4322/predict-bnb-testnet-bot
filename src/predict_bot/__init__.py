c47c9e6801b5732f88bc645007323710bcc91ee8
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-07 06:55:28 +0800

    Install decision controller live whitelist bridge

--- src/predict_bot/__init__.py
+++ src/predict_bot/__init__.py
@@ -27,6 +27,9 @@ from .decision_snapshot_diagnostics import (
 from .decision_snapshot_state import (
     install_decision_snapshot_state as _install_decision_snapshot_state,
 )
+from .decision_strategy_live_v2 import (
+    install_decision_strategy_live_v2 as _install_decision_strategy_live_v2,
+)
 from .decision_strategy_native_v2 import (
     install_decision_strategy_native_v2 as _install_decision_strategy_native_v2,
 )
@@ -154,12 +157,14 @@ _install_live_rules_async_preflight_patch()
 _install_live_strong_trend_guard_patch()
 _install_regime_reverse_prediction_age_patch()
 _install_decision_strategy_native_v2()
+_install_decision_strategy_live_v2()
 del _install_calibrated_value_confirm_v2_live_patch
 del _install_calibrated_value_confirmation_filters
 del _install_calibrated_value_confirmation_variants
 del _install_confirmation_add_sources_patch
 del _install_decision_snapshot_diagnostics
 del _install_decision_snapshot_state
+del _install_decision_strategy_live_v2
 del _install_decision_strategy_native_v2
 del _install_direct_outcome_guard
 del _install_live_rules_async_preflight_patch
