93b298b646804cf699c649a5d85009118b0e097e
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-07 04:42:57 +0800

    Install native decision strategy patch

--- src/predict_bot/__init__.py
+++ src/predict_bot/__init__.py
@@ -27,6 +27,9 @@ from .decision_snapshot_diagnostics import (
 from .decision_snapshot_state import (
     install_decision_snapshot_state as _install_decision_snapshot_state,
 )
+from .decision_strategy_shadows import (
+    install_decision_strategy_shadows as _install_decision_strategy_shadows,
+)
 from .direct_outcome_guard import (
     install_direct_outcome_guard as _install_direct_outcome_guard,
 )
@@ -150,12 +153,14 @@ _install_maximum_net_loss_guard_patch()
 _install_live_rules_async_preflight_patch()
 _install_live_strong_trend_guard_patch()
 _install_regime_reverse_prediction_age_patch()
+_install_decision_strategy_shadows()
 del _install_calibrated_value_confirm_v2_live_patch
 del _install_calibrated_value_confirmation_filters
 del _install_calibrated_value_confirmation_variants
 del _install_confirmation_add_sources_patch
 del _install_decision_snapshot_diagnostics
 del _install_decision_snapshot_state
+del _install_decision_strategy_shadows
 del _install_direct_outcome_guard
 del _install_live_rules_async_preflight_patch
 del _install_live_strong_trend_guard_patch
