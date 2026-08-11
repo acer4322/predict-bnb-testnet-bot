b93cea88083e32a30a1fcbf363de6ecacbd56e2f
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-11 22:46:37 +0800

    Add Dashboard V2 React entrypoint

---
+++ dashboard-v2/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ConfigProvider, theme } from 'antd'
+import zhTW from 'antd/locale/zh_TW'
+import App from './App'
+import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ConfigProvider
+      locale={zhTW}
+      theme={{
+        algorithm: theme.defaultAlgorithm,
+        token: {
+          borderRadius: 10,
+          colorBgLayout: '#f3f5f8',
+          fontFamily: 'Inter, "Noto Sans TC", "Microsoft JhengHei", system-ui, sans-serif',
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>,
+)
