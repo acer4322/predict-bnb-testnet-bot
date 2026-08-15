f28ad9f1a0b075008a20c8c12a8ab0ed3ca32761
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-16 02:25:01 +0800

    Wire service controls into Diagnostics

---
+++ dashboard-v2/src/diagnostics-service-control-page.tsx
@@ -0,0 +1,11 @@
+import { DiagnosticsPage } from './pages-v2'
+import ServiceControlPanel from './service-control-panel'
+
+export default function DiagnosticsServiceControlPage() {
+  return (
+    <>
+      <ServiceControlPanel />
+      <DiagnosticsPage />
+    </>
+  )
+}
