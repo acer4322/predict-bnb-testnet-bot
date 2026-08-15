3f2cb84ea6b0c51a8bd24b5e3cafb7a7ca05c0b2
acer4322 <40157406+acer4322@users.noreply.github.com>
2026-08-16 06:12:26 +0800

    Show 8782 EBM strategy test in Diagnostics

--- dashboard-v2/src/diagnostics-service-control-page.tsx
+++ dashboard-v2/src/diagnostics-service-control-page.tsx
@@ -1,10 +1,12 @@
 import { DiagnosticsPage } from './pages-v2'
 import ServiceControlPanel from './service-control-panel'
+import EbmStrategyTestServiceCard from './ebm-strategy-test-service-card'
 
 export default function DiagnosticsServiceControlPage() {
   return (
     <>
       <ServiceControlPanel />
+      <EbmStrategyTestServiceCard />
       <DiagnosticsPage />
     </>
   )
